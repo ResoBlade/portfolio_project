35077687892e45f5284e835019f5296c43dc45a7
ResoBlade <jhenlov@gmail.com>
2022-07-12 11:04:53 +0200

    mobile nav start

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -17,13 +17,31 @@ const Navbar = () => {
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
           <div />
-          <a href={`#$(item)`}>{item}</a>
+          <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
 
       <div className="app__navbar-menu">
-      <HiMenuAlt4 onClick={() => setToggle(false)} />
+      <HiMenuAlt4 onClick={() => setToggle(true)} />
+      {toggle && (
+        <motion.div
+        whileInView={{ x: [300, 0] }}
+        transition={{ duration: 0.85, ease: 'easeOut'}}
+        >
+          <HiX onClick={() => setToggle(false)} />
+          <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+        </motion.div>
+      )
+      }
 
       </div>
       </nav>
