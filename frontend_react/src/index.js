6c693306d8d58cebd251f7c05d6edff98ae66d67
ResoBlade <jhenlov@gmail.com>
2023-01-06 15:23:03 +0100

    higher order components

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,3 +1,7 @@
+// https://youtu.be/3HNyXCPDQ7Q?t=7034
+// https://github.com/adrianhajdin/project_professional_portfolio/
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -5,3 +9,6 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
