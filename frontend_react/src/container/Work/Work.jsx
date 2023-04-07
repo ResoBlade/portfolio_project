8f6e8e9b1c38dea7adee1f3aea364be6637d047d
ResoBlade <jhenlov@gmail.com>
2023-04-07 20:40:46 +0200

    back into it. Middle of Work.jsx

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -54,6 +54,17 @@ const Work = () => {
               className="app__work-img app__flex"
             >
               <img src={urlFor(work.imgUrl)} alt={work.name} />
+              <motion.div
+              whileHover={{opacity: [0, 1]}}
+              transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+              className="app__work-hover app__flex"
+              
+              >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                  
+                </a>
+
+              </motion.div>
             </div>
           </div>
         ))}
