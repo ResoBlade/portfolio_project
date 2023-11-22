9fbf7d55c255e9754de4f0f80bab688f217dd44c
ResoBlade <jhenlov@gmail.com>
2023-11-22 15:34:49 +0100

    Testimonials and debugging

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -7,10 +7,10 @@ const NavigationDots = ({ active }) => (
         href={`#${item}`}
         key={item + index}
         className="app__navigation-dot"
-        style={active === item ? { backgroundColor: '313BAC' } : {}}
+        style={active === item ? { backgroundColor: '#313BAC' } : {}}
       />
     ))}
   </div>
 );
 
-export default NavigationDots;
+export default NavigationDots; 
